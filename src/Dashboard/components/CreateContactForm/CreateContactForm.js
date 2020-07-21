2c9625e9eb44fcfa810b47fd444702ed8dd2a8b2
Steven Becker <51137960+stevelagarto@users.noreply.github.com>
2020-07-21 14:35:54 +0200

    chore: add style and messages to create contact form (#6)

--- src/Dashboard/components/CreateContactForm/CreateContactForm.js
+++ src/Dashboard/components/CreateContactForm/CreateContactForm.js
@@ -1,9 +1,11 @@
 import React, { useReducer } from 'react'
 import PropTypes from 'prop-types'
 
+import styles from './style.module.css'
+
 import { reducer, initialFormValues, setField } from './reducer'
 
-const CreateContactForm = ({ createContact, toggleCreateContactForm }) => {
+const CreateContactForm = ({ createContact, toggleCreateContactForm, errorMessage }) => {
   const [formValues, dispatch] = useReducer(reducer, initialFormValues)
 
   function handleOnChange (event) {
@@ -16,45 +18,65 @@ const CreateContactForm = ({ createContact, toggleCreateContactForm }) => {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        name="first_name"
-        value={formValues.first_name}
-        onChange={handleOnChange}
-        required
-        type="text"
-      />
-      <input
-        name="last_name"
-        value={formValues.last_name}
-        onChange={handleOnChange}
-        required
-        type="text"
+    <div className={styles.container}>
 
-      />
-      <input
-        name="email"
-        value={formValues.email}
-        onChange={handleOnChange}
-        required
-        type="email"
-      />
-      <input
-        name="phone"
-        value={formValues.phone}
-        onChange={handleOnChange}
-        required
-        type="number"
-      />
-      <input type="submit" />
-      <input type="button" value="Cancel" onClick={toggleCreateContactForm} />
-    </form>
+      <form onSubmit={handleSubmit} className={styles.form}>
+        <div className={styles.title}>ADD CONTACT</div>
+        <input
+          name="first_name"
+          value={formValues.first_name}
+          onChange={handleOnChange}
+          required
+          type="text"
+          className={styles.textInput}
+          placeholder="First Name"
+        />
+        <input
+          name="last_name"
+          value={formValues.last_name}
+          onChange={handleOnChange}
+          required
+          type="text"
+          className={styles.textInput}
+          placeholder="Last Name"
+        />
+        <input
+          name="email"
+          value={formValues.email}
+          onChange={handleOnChange}
+          required
+          type="email"
+          className={styles.textInput}
+          placeholder="Email"
+        />
+        <input
+          name="phone"
+          value={formValues.phone}
+          onChange={handleOnChange}
+          required
+          type="number"
+          className={styles.textInput}
+          placeholder="Phone"
+        />
+        <div className={styles.buttonContainer}>
+          <input type="submit" className={styles.button} />
+          <input
+            type="button"
+            value="Cancel"
+            onClick={toggleCreateContactForm}
+            className={styles.button}
+          />
+        </div>
+        <div className={styles.errorMessage}>{errorMessage}</div>
+      </form>
+    </div>
   )
 }
 
 CreateContactForm.propTypes = {
   createContact: PropTypes.func,
-  toggleCreateContactForm: PropTypes.func
+  toggleCreateContactForm: PropTypes.func,
+  errorMessage: PropTypes.string
 }
 
 export default CreateContactForm
