7a06d02fcdbc6a8a71b87964e7fa03803cca8b71
stevelagarto <stevenzgz@gmail.com>
2020-07-21 14:33:29 +0200

    chore: add style and messages to create contact form

--- src/Dashboard/DashboardContainer.js
+++ src/Dashboard/DashboardContainer.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react'
 
 import { getAllContacts, createContact } from '../services/api'
 
+import styles from './style.module.css'
+
 import ContactList from './components/ContactList/ContactList'
 import CreateContactForm from './components/CreateContactForm/CreateContactForm'
 import CreateContactButton from './components/CreateContactButton/CreateContactButton'
@@ -9,6 +11,8 @@ import { sortContacts } from '../services/sortContacts'
 
 const DashboardContainer = () => {
   const [contacts, setContacts] = useState([])
+  const [errorMessage, setErrorMessage] = useState('')
+  const [message, setMessage] = useState('')
   const [showCreateContactForm, setShowCreateContactForm] = useState(false)
 
   useEffect(() => {
@@ -17,23 +21,32 @@ const DashboardContainer = () => {
 
   const handleCreateContact = async (contact) => {
     const createdContact = await createContact(contact)
-    if (Object.prototype.hasOwnProperty.call(createdContact, 'error')) alert(createdContact.error)
+    if (Object.prototype.hasOwnProperty.call(createdContact, 'error')) setErrorMessage(createdContact.error)
     else {
       setContacts(sortContacts([...contacts, createdContact]))
       toggleCreateContactForm()
-      console.log('Contact Created: ', createdContact)
+      setMessage('Contact added Succesfully')
     }
   }
 
   const toggleCreateContactForm = () => {
+    setErrorMessage('')
+    setMessage('')
     setShowCreateContactForm(value => !value)
   }
 
   return contacts.length > 0 ? (
     <>
       { showCreateContactForm
-        ? <CreateContactForm createContact={handleCreateContact} toggleCreateContactForm= {toggleCreateContactForm} /> : <CreateContactButton toggleCreateContactForm= {toggleCreateContactForm} />
+        ? <CreateContactForm
+          createContact={handleCreateContact}
+          errorMessage={errorMessage}
+          toggleCreateContactForm= {toggleCreateContactForm}
+        />
+        : null
       }
+      <CreateContactButton toggleCreateContactForm= {toggleCreateContactForm} />
+      <div className={styles.message}>{message}</div>
       <ContactList contacts ={contacts}/>
     </>
   ) : <div>LOADING</div>
