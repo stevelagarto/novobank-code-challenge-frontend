ea71359a29ecc6accdc20732b80e511ad10dc46c
stevelagarto <stevenzgz@gmail.com>
2020-07-21 20:29:34 +0200

    chore: add style to contact list

--- src/Dashboard/components/ContactList/ContactItem/ContactItem.js
+++ src/Dashboard/components/ContactList/ContactItem/ContactItem.js
@@ -1,8 +1,17 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import styles from './style.module.css'
+
 const ContactItem = ({ contact }) => {
-  return (<div>{contact.first_name}</div>)
+  return (
+    <>
+      <div className={styles.gridItem}>{contact.first_name}</div>
+      <div className={styles.gridItem}>{contact.last_name}</div>
+      <div className={styles.gridItem}>{contact.email}</div>
+      <div className={styles.gridItem}>{contact.phone}</div>
+    </>
+  )
 }
 
 ContactItem.propTypes = {
